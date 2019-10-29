bcb58c1f8fadd46cc0571ee22b190950c40c45d0
Jose <j.a.ojeda@live.com>
2019-10-29 16:25:33 -0700

    added Character.css

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Character.css';
 
 const Character = ({ img, name, species }) => (
-  <dl>
+  <dl  className={styles.Character}>
     <dt>Name</dt>
     <dd>{name}</dd>
 
