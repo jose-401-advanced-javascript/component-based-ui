557756ca43b6f93a954fa71d5a7714ccc52a0ea2
Jose <j.a.ojeda@live.com>
2019-10-29 16:10:44 -0700

    added different characters to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,42 @@ const App = () => (
       name="Rick"
       img="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
       species="Human" />
+    <Character
+      name="Cornvelious Daneil"
+      img="https://rickandmortyapi.com/api/character/avatar/150.jpeg"
+      species="Gromflomite" />
+    <Character
+      name="Lil B"
+      img="https://rickandmortyapi.com/api/character/avatar/203.jpeg"
+      species="Snail Alien" />
+    <Character
+      name="Trandor"
+      img="https://rickandmortyapi.com/api/character/avatar/363.jpeg"
+      species="Krootabulon" />
+    <Character
+      name="Shmlamantha Shmlicelli"
+      img="https://rickandmortyapi.com/api/character/avatar/314.jpeg"
+      species="Human" />
+    <Character
+      name="Cynthia"
+      img="https://rickandmortyapi.com/api/character/avatar/88.jpeg"
+      species="Human" />
+    <Character
+      name="Photography Cyborg"
+      img="https://rickandmortyapi.com/api/character/avatar/261.jpeg"
+      species="Robot" />
+    <Character
+      name="Shnoopy Bloopers"
+      img="https://rickandmortyapi.com/api/character/avatar/320.jpeg"
+      species="Alien" />
+    <Character
+      name="Armagheadon"
+      img="https://rickandmortyapi.com/api/character/avatar/24.jpeg"
+      species="Cromulon" />
+    <Character
+      name="David Letterman"
+      img="https://rickandmortyapi.com/api/character/avatar/91.jpeg"
+      species="Human" />
     <Footer />
   </>
 );
