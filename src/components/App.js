de7d27d733de87e16b25477f6c17a2c1cab05079
Jose <j.a.ojeda@live.com>
2019-10-29 15:23:43 -0700

    moved Footer to component file

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import Header from './Header';
-
-const Footer = () => (
-  <footer>&copy; MEEEE</footer>
-);
+import Footer from './Footer';
 
 const App = () => (
   <>
